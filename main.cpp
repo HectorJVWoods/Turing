c39355779eb39279afb454ce878ce353ae71f073
HJVWoods <hjvw500@york.ac.uk>
2022-04-06 00:46:03 +0100

    GeneratorTypes and Operators

--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
 #include "Types.cpp"
 
+
+string intGenerator(int x){
+    string outputStr = "";
+    while(x > 0){
+        outputStr = outputStr + "1";
+        x = x - 1;
+    }
+    return outputStr;
+}
+
+bool numberFiveTypeAcceptor(int x){
+    return x == 5;
+}
+
+int fiveOperator(int x, int y){
+    if(x == y) {
+        return 5;
+    }
+    return -5;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    TuringType abc{8}; //  8-bit type so supports 2^8 different values
-    abc.setName("hey");
+    //GeneratorType int8{8};
+    GeneratorType numberFive8{8};
+    //int8.setGenerator(intGenerator);
+    //std::cout << int8.generate(450);
+    //numberFive8.setAcceptor(numberFiveTypeAcceptor);
+    numberFive8.defineOperator("++", fiveOperator);
+    //std::cout << numberFive8.isMember(56);
+    //std::cout << numberFive8.isMember(5);
 
-    int x = 0b0001 + 1;
-
-
-    std::cout << abc.name;
+    std::cout << numberFive8.operate("++", 5, 5);
+    std::cout << numberFive8.operate("++", 5, 6);
+    //std::cout << abc.name;
 
     return 0;
 }
